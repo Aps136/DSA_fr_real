ffc8da0adf8709aeb9aee89b13be9ea0dcada01c
K V Aparna Karanth <aparna1.mitmpl2023@learner.manipal.edu>
2026-06-22 20:47:40 +0530

    Create Happy_Number.cpp

---
+++ Hashmap/Happy_Number.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool isHappy(int n) {
+        unordered_set<int> st;
+        while(st.find(n)== st.end()){
+            st.insert(n);
+            n = getnext(n);
+            if(n==1){
+                return 1;
+            }
+        }
+        return false;        
+    }
+private:
+    int getnext(int n){
+        
+        int sum=0;
+       
+        while(n>0){
+            int x = n%10;
+            sum+=x*x;
+            n/=10;
+        }
+        return sum;
+    }
+};
+
+//TC: O(logn), SC: O(k)
