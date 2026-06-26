c5e810435e5eecddbfa3c69e6ea9c6b602e7b9fa
K V Aparna Karanth <aparna1.mitmpl2023@learner.manipal.edu>
2026-06-26 23:15:51 +0530

    Create first_occurrence.cpp

---
+++ Arrays/first_occurrence.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        if(needle.length()> haystack.length()){
+            return -1;
+        }
+        for(int i=0;i<=(haystack.length()-needle.length());i++){
+            if(haystack.substr(i,needle.length())==needle){
+                return i;
+
+            }
+
+        }
+        return -1;
+        
+    }
+};
+
+//TC: O(N-M)*M
+//SC: O(M)
