61953b85642e8f36812d5def8fb226a82ffb59e2
K V Aparna Karanth <aparna1.mitmpl2023@learner.manipal.edu>
2026-07-06 21:44:17 +0530

    Create Min_Abs_Diff.cpp

---
+++ BST/Min_Abs_Diff.cpp
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int mini= INT_MAX;
+    TreeNode* prev = NULL;
+    void dfs(TreeNode* root){
+            if(root->left) dfs(root->left);
+            if(prev)  mini = min(mini, abs(prev->val - root->val));
+            prev = root;
+            if(root->right) dfs(root->right);
+        }
+    int getMinimumDifference(TreeNode* root) {
+        dfs(root);
+        return mini;       
+    }
+};
+
+
+//TC: O(n) SC: O(h)
