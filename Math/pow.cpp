7a8633c4a703a63eb6f5d10da1393418db908947
K V Aparna Karanth <aparna1.mitmpl2023@learner.manipal.edu>
2026-07-11 07:26:38 +0530

    Update pow.cpp

--- Math/pow.cpp
+++ Math/pow.cpp
@@ -1,26 +1,15 @@
 class Solution {
 public:
-    double myPow(double x, int n) {
+    double myPow(double x, long long n) {
         if(n>0){
-            if(n==1){
-                return x;
-            }
-            if(n%2==0){
-                double a = myPow(x,n/2);
-                return a*a;
-            }else{
-                double a = myPow(x,n/2);
-                return a*a*x;
-            }
-
-        }else if(n==0){
+            if(n==1) return x;
+            double a = myPow(x,n/2);
+            return (n%2==0)? a*a : a*a*x;
+        }
+        else if(n==0){
             return 1;
         }else{
             return myPow(1/x, -n);
         }
-
-        
     }
 };
-
-//TC: O(LOGN) SC: O(LOGN)
