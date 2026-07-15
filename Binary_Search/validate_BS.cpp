0178a2c48cf8f55dca8db7b3c947915a8cfd23fe
K V Aparna Karanth <aparna1.mitmpl2023@learner.manipal.edu>
2026-07-15 17:08:06 +0530

    Create validate_BS.cpp

---
+++ Binary_Search/validate_BS.cpp
@@ -0,0 +1,28 @@
+show me with example how this recursion happens: /**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isValidBST(TreeNode* root) {
+         return valid(root, LONG_MIN, LONG_MAX);       
+        
+    }    
+private: 
+    bool valid(TreeNode* node, long mini, long maxi){
+        if(!node) return true;
+        if(!(node->val > mini && node->val<maxi)) return false;
+        return valid(node->left, mini, node->val) && valid(node->right, node->val, maxi);
+    }
+
+}; - 
+
+
+//TC: O(N) SC: O(N) 
