76e911a4732deeb4dce4d79456f3b1602eb66bb2
K V Aparna Karanth <aparna1.mitmpl2023@learner.manipal.edu>
2026-07-22 08:46:16 +0530

    Create Mini_Size_Subarray_Sum.cpp

---
+++ Sliding_Window/Mini_Size_Subarray_Sum.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int minSubArrayLen(int target, vector<int>& nums) {        
+        int i=0;
+        int j=0;        
+        int n = nums.size();
+        int mini= INT_MAX;
+        int sum =0;
+        while(j<n){
+            sum+=nums[j];
+            while(sum>=target){
+                mini = min(mini, j-i+1);
+                sum-=nums[i];
+                i++;
+            }
+            j++;
+        }
+        return mini==INT_MAX? 0:mini;
+        
+    }
+};
+
+// TC: O(N) SC: O(1)
