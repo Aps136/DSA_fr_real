f8cf73a64e61ac3e257829cbe69604c856633582
K V Aparna Karanth <aparna1.mitmpl2023@learner.manipal.edu>
2026-08-18 14:03:45 +0530

    Create quick_sort.cpp

---
+++ Sorting/quick_sort.cpp
@@ -0,0 +1,36 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+int partition(vector<int> &arr, int low, int high){
+    int pivot=arr[high];
+    int i=low-1;
+    for(int j=low;j<high;++j){
+        if(arr[j]<=pivot){
+            i++;
+            swap(arr[i],arr[j]);
+
+        }
+    }
+    swap(arr[i+1],arr[high]);
+    return i+1;
+}
+void quickSort(vector<int> &arr, int low, int high){
+    if(low<high){
+        int pi = partition(arr, low, high);
+        quickSort(arr,low,pi-1);
+        quickSort(arr,pi+1,high);
+    }
+}
+int main() {
+    vector<int> arr={10, 80, 30, 90, 40, 50, 70};
+    quickSort(arr,0, arr.size()-1);
+    cout<<"Sorted Array"<<endl;
+    for(int x: arr){
+        cout<<x<<" ";
+    }
+    cout<<endl;
+    return 0;
+}
+
+//TC: O(NLOGN) SC: O(LOGN) 
