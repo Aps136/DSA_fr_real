0c28111606acb87b7de504af352aa0c5c4f5a98a
K V Aparna Karanth <aparna1.mitmpl2023@learner.manipal.edu>
2026-08-23 11:53:42 +0530

    Create Next_greater_el_I.cpp

---
+++ Stack/Next_greater_el_I.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        unordered_map<int,int> mp;
+        stack<int> st;
+        for(int x: nums2){
+            while(!st.empty() && x>st.top()){
+                mp[st.top()]=x;
+                st.pop();
+            }
+            st.push(x);
+                    
+        }
+        while(!st.empty()){
+                mp[st.top()]=-1;
+                st.pop();
+
+            }   
+        vector<int>res;
+            for(int y: nums1){
+                res.push_back(mp[y]);
+            }
+        return res;
+    }
+};
+
+//TC: O(N+M) SC: O(M) 
